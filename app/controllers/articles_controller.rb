18c114a5db5d373974d7e3e1543adb3d32b3fdb0
Quentinou <quentinsyrt@gmail.com>
2018-10-16 17:33:21 +0000

    STOP répétitions article_controllers -> before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
     
     def index
         @articles = Article.all
@@ -10,7 +12,6 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
     end
     
     
@@ -27,7 +28,6 @@ class ArticlesController < ApplicationController
     
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article bien modifié"
             redirect_to article_path(@article)
@@ -37,11 +37,9 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id])
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article supprimé !"
         
@@ -52,6 +50,10 @@ class ArticlesController < ApplicationController
     private
     def article_params
        params.require(:article).permit(:title, :description) 
+    end
+    
+    def set_article
+        @article = Article.find(params[:id])
     end
     
 end
